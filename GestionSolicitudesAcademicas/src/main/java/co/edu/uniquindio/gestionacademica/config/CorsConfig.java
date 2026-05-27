dc97f8712765618cb00d7e2f0b5de0df41ec7b74
SantiagoLeonE <santiago.leone@uqvirtual.edu.co>
2026-05-27 18:55:28 -0500

    fix: Se agrego la url dada por render en el frontend para que este apunte correctamente al backend

--- GestionSolicitudesAcademicas/src/main/java/co/edu/uniquindio/gestionacademica/config/CorsConfig.java
+++ GestionSolicitudesAcademicas/src/main/java/co/edu/uniquindio/gestionacademica/config/CorsConfig.java
@@ -21,7 +21,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         //Permitir peticiones desde el frontend Angular
-        config.setAllowedOrigins(List.of(allowedOrigins));
+        config.setAllowedOrigins(List.of(allowedOrigins, "https://TU-FRONTEND.onrender.com"));
 
         //Métodos HTTP permitidos
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
